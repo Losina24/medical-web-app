cb7f0570c71451efd39fac87718809529c10743e
Losina <losina.dd@gmail.com>
2021-05-01 17:16:28 +0200

    New version

--- medical-web-app/medical-web-app/Properties/AssemblyInfo.cs
+++ medical-web-app/medical-web-app/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("471281f6-9335-4221-af37-4fe2b7dadf3d")]
+[assembly: Guid("cc36400d-b445-4ade-8015-2f2a871243ea")]
 
 // Version information for an assembly consists of the following four values:
 //
